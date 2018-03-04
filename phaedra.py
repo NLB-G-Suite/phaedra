e5add1b23c6a99b428cefb171dd7478811858bc9
Marcel Schwittlick <info@schwittlick.net>
2018-03-04 22:31:03 +0100

    save quit

--- phaedra.py
+++ phaedra.py
@@ -2,11 +2,13 @@ import pyscreenshot as ImageGrab
 import codecs
 from PIL import Image
 import pytesseract
+import time
 
 if __name__ == '__main__':
     while True:
-        im = ImageGrab.grab(bbox=(280, 200, 960, 600))
+        im = ImageGrab.grab(bbox=(280, 200, 1160, 600))
         text = pytesseract.image_to_string(im)
-        file = codecs.open('text.txt', "a", "utf-8")
+        file = codecs.open('text7.txt', "a", "utf-8")
         file.write(text)
         file.close()
+        time.sleep(0.05)

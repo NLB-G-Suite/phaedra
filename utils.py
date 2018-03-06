cda2a1cc4ad6652028284f83fc3005fe99612cb0
Marcel Schwittlick <info@schwittlick.net>
2018-03-06 16:49:55 +0100

    adds code to save

--- utils.py
+++ utils.py
@@ -4,3 +4,14 @@ def in_blocklist(blocklist, sentence):
             return True
         
     return False
+
+
+def save_all(filename, authors):
+    file = open(filename, 'w')
+    for author, sentences in authors.iteritems():
+        for sentence in sentences:
+            file.write(author + ': ' + sentence)
+            file.write('\n')
+
+    file.close()
+    print('Saved to ' + filename)
